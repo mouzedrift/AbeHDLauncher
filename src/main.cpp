0a07797c255eea1b093e92283228e83a80c64e99
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2026-04-26 22:23:30 +0200

    add and load custom fonts

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include "mainwindow.hpp"
 #include <QApplication>
 #include <QMessageBox>
+#include <QFontDatabase>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+    
+    const char* customFontFiles[] = {"NewPortLandOpti-Regular.otf", "URW Contemporary Brush W01 Bd.ttf"};
+    for (const char* fontFile : customFontFiles)
+    {
+        QString fontPath = QString(":/fonts/%1").arg(fontFile);
+        int fontId = QFontDatabase::addApplicationFont(fontPath);
+        if (fontId == -1)
+        {
+            qDebug() << "custom font failed to load:" << fontPath;
+        }
+    }
 
     QString aeInstallPath = MainWindow::FindGameInstallPath(eGameType::eAE);
     QString aoInstallPath = MainWindow::FindGameInstallPath(eGameType::eAO);
